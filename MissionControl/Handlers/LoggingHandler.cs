1e2e527539146908470418e06f3a3ee13ff05842
uneverdreamed <ma2005f@gmail.com>
2026-03-11 22:10:56 +0300

    MissionControl | Handlers | Добавлен обработчик логирования, который записывает команду в журнал миссии

---
+++ MissionControl/Handlers/LoggingHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandCore.Base;
+using CommandCore.Models;
+
+namespace MissionControl.Handlers
+{
+    public class LoggingHandler : BaseCommandHandler
+    {
+        protected override void ProcessCommand(CommandContext context)
+        {
+            var cmd = context.ParsedCommand!;
+            string logEntry = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] " +
+                             $"Команда: {cmd} | " +
+                             $"Батарея: {context.ProbeState.BatteryLevel}% | " +
+                             $"Режим: {(context.ProbeState.IsInSafeMode ? "SAFE" : "NORMAL")}";
+
+            context.AddLog($"[LOG] Записано в журнал миссии");
+            context.AddLog($"[LOG]   └─ {logEntry}");
+        }
+    }
+}
