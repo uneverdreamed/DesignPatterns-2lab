4ea104161f59b08be5c9720a7d564629de78ba73
uneverdreamed <ma2005f@gmail.com>
2026-03-11 19:15:46 +0300

    CommandCore | Models | создана модель команды космического зонда Command.cs

---
+++ CommandCore/Models/Command.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandCore.Models
+{
+ // модель команды космического зонда  
+    public class Command
+    {
+        public string Type { get; set; } = string.Empty; // тип команды
+        public string Parameter { get; set; } = string.Empty; // параметр команды
+        public DateTime Timestamp { get; set; } // время создания команды
+        public Command(string type, string? parameter=null)
+        {
+            Type = type;
+            Parameter = parameter;
+            Timestamp = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return Parameter != null ? $"{Type} {Parameter}" : Type;
+        }
+
+    }
+}
