5364dc2c265250644efed76740340c474f3c8b2f
uneverdreamed <ma2005f@gmail.com>
2026-03-11 22:13:36 +0300

    MissionControl | Handlers | добавлен обработчик шифрования, который шифрует данные перед их передачей

---
+++ MissionControl/Handlers/EncryptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandCore.Base;
+using CommandCore.Models;
+
+namespace MissionControl.Handlers
+{
+    class EncryptionHandler
+    {
+		public class EncryptionHandler : BaseCommandHandler
+		{
+			private const byte EncryptionKey = 0x5A; 
+
+			protected override void ProcessCommand(CommandContext context)
+			{
+				context.AddLog("[ENCRYPT] Шифрование данных...");
+
+				if (context.TransformedData == null)
+				{
+					context.AddLog("[ENCRYPT] Нет данных для шифрования, пропуск");
+					return;
+				}
+				var encrypted = new byte[context.TransformedData.Length];
+				for (int i = 0; i < context.TransformedData.Length; i++)
+				{
+					encrypted[i] = (byte)(context.TransformedData[i] ^ EncryptionKey);
+				}
+
+				context.TransformedData = encrypted;
+
+				var hexString = BitConverter.ToString(encrypted).Replace("-", " ");
+				context.AddLog($"[ENCRYPT] Данные зашифрованы: [{hexString}]");
+			}
+}
