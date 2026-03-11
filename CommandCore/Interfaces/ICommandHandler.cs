c6ead4f5ce0bc063e2c11ee400f688c5e64b1360
uneverdreamed <ma2005f@gmail.com>
2026-03-11 20:30:42 +0300

    CommandCore | Interfaces | создан интерфейс ICommandHandler для обработчика команд в цепочке ответственности

---
+++ CommandCore/Interfaces/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandCore.Models;
+
+namespace CommandCore.Interfaces
+{
+    // интерфейс для обработчика команды в цепочке ответственности
+    public interface ICommandHandler
+    {
+        void SetNext(ICommandHandler next); // установка следующего обработчика в цепочке
+        CommandContext Handle(CommandContext context); // обработка команды и передача дальше по цепочке
+    }
+}
